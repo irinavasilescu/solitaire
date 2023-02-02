a70f9a739bf3a9e82a1a3ad32e84c789798ecb90
Irina Vasilescu <irina>
2023-02-02 22:44:15 +0200

    initDisplayedCards, initGame

--- lib/game.dart
+++ lib/game.dart
@@ -9,20 +9,40 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> {
   List<PlayingCard> deck = standardFiftyTwoCardDeck();
 
+  late List<PlayingCard> displayedCards;
+  late List<PlayingCard> remainingCards;
+  late List<PlayingCard> discardedCards;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    initGame();
+  }
+
+  void initGame() {
+    deck.shuffle();
+    initDisplayedCards();
+  }
+
+  void initDisplayedCards() {
+    setState(() {
+      displayedCards = deck.take(25).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    deck.shuffle();
-
     return GridView.count(
       crossAxisCount: 5,
       children: [
-        for ( var card in deck )
-          Container(
-            height: 75,
-            child: PlayingCardView(
-              card: PlayingCard(card.suit, card.value)
-            )
+        for (var card in displayedCards)
+        Container(
+          height: 75,
+          child: PlayingCardView(
+            card: PlayingCard(card.suit, card.value)
           )
+        )
       ]
     );
   }
